613495ba445a03a5f07ee44beaad4f8b2e99ad57
mdmartinvega <mdmartinvega@gmail.com>
2021-03-29 21:43:16 +0200

    Ejercicios sintaxis basica hasta el 16

--- JavaScript/Ejercicios/Sintaxis_basica/js/main.js
+++ JavaScript/Ejercicios/Sintaxis_basica/js/main.js
@@ -76,3 +76,136 @@ console.log("********Apdo 8*****************");
 /*8. Crea una función que reciba un array y devuelva su 
 último elemento. Prueba tu función con varios arrays
 de diferentes longitudes.*/
+
+const myArray3 = function(array) {
+    
+    return array[length-1];
+}
+console.log(myArray3([1, 2, 3]));
+
+console.log("********Apdo 9*****************");
+/* 9. Un granjero necesita contar la cantidad de patas de los animales en susdiferentes granjas muy
+frecuentemente, pero tiene tres especies: pollos (2 patas), vacas (4 patas) y 
+cerdos (4 patas). Tu función recibirá la cantidad de animales en ese orden y devolverá la cantidad de patas totales.*/
+
+const countLegs = function(value1, value2, value3) {
+    let pollos = value1 * 2;
+    let vacas = value2 * 4;
+    let cerdos = value3 * 4;
+    return pollos + vacas + cerdos;
+}
+console.log(`El número de animales en la granja es de ${countLegs(2, 2, 2)}`);
+
+console.log("********Apdo 10*****************");
+/*10. Crea una función que determine si dos datos recibidos por argumentos 
+son del mismo tipo.*/
+
+let sameType = (data1, data2) =>
+typeof data1 === typeof data2 ? console.log(`Mismo tipo`) : console.log(`Diferente tipo`);
+sameType(1, 1);
+
+console.log("********Apdo 11*****************");
+/*11. Crea una función que reciba un string con una frase y devuelva un array 
+donde cada elemento será una palabra de la frase original. Investigar método 
+existente de los strings para este fin.*/
+
+const splitFunction = value =>
+    value.split(" ");
+console.log(splitFunction("Hoy es lunes"));
+
+console.log("********Apdo 12*****************");
+/*12. Inicializa dos objetos, address1 y address2 con las 
+propiedades: provincia, ciudad, municipio, código
+postal, calle, número, planta, y número de puerta.*/
+
+const address1 = {
+    provincia: "Madrid",
+    ciudad: "Madrid",
+    municipio: "Alcobendas",
+    codigo_postal: 28721,
+    calle: "Velazquez",
+    numero: 3,
+    planta: 6,
+    num_puerta: "A"
+}
+console.log(address1);
+
+const address2 = {
+    provincia: "Barcelona",
+    ciudad: "Barcelona",
+    municipio: "Sitges",
+    codigo_postal: 08721,
+    calle: "Eras",
+    numero: 1,
+    planta: 9,
+    num_puerta: "B"
+}
+console.log(address2);
+
+console.log("********Apdo 13*****************");
+/*13. Los dominios en la web, se componen del nombre de dominio (codespaceacademy) 
+y de un TLD (toplevel domain) como, por ejemplo .com / .es / .org, etc. Crea 
+una función que se llame parseDomain() que reciba por argumento un string y 
+devuelva un objeto con dos propiedades: domain y tld.*/
+
+const parseDomain = function(value) {
+    let domain, tld;
+    value.split(".");
+    return {domain, tld};
+}
+console.log(parseDomain("codespaceacademy.com"));
+
+console.log("********Apdo 14*****************");
+/*14. Nos han prohibido el uso del operador de igualdad estricta (===), pero 
+queremos poder seguir utilizando dicha esa funcionalidad. Crea una función que
+devuelva true si dos números tienen el mismo valor y el mismo tipo de dato.
+Debemos usar el operador lógico “&&”. Prueba tu función con dos estos inputs:*/
+
+let strictEquality = function (value3, value4) {
+    if (value3 == value4 && typeof value3 == typeof value4) {
+        console.log(true);
+    } else {
+        console.log(false);
+    }
+}
+strictEquality(5,5);
+
+console.log("********Apdo 15*****************");
+/*15. Crea una función que reciba dos strings y devuelva true si tienen la 
+misma longitud y false en caso contrario.*/
+
+const sameString = (value1, value2) =>
+    value1.length === value2.length ? console.log(true): console.log(false);
+sameString("hola", "hhola");
+
+console.log("********Apdo 16*****************");
+/*16. Crea una función que reciba un string y determine si está vacío sin 
+utilizar la propiedad length.*/
+
+// const emptyArray = function(value) {
+//     if (value.empty()) {
+//         console.log("Está vacío");
+
+//     } else {
+//         console.log("No está vacío");
+//     }
+// };
+// emptyArray();
+
+console.log("********Apdo 17*****************");
+/*17. Imprimir elemento a elemento el array del apartado 1 de cuatro formas 
+diferentes: */
+myArray = ["one", "two", "three", "four", "five"];
+//• while
+
+//• for
+for (let i=0;i < myArray.length; i++) {
+    console.log(`El índice es ${i} y el valor es ${myArray[i]}`);
+}
+
+//• for of
+
+//• forEach.*/
+myArray.forEach((value, index) => {
+    console.log(`El índice es ${index} y el valor es ${value}`); 
+});
