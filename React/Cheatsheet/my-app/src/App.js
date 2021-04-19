08f3081eac50149d510d13e514b967b59e3f0b3f
mdmartinvega <mdmartinvega@gmail.com>
2021-04-19 13:56:49 +0200

    Comenzamos React

---
+++ React/Cheatsheet/my-app/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import FirstComponent from "./components/FirstComponent"
+function App() {
+  return (
+    <div className="App">
+      <FirstComponent />
+      <FirstComponent />
+      <FirstComponent />
+      <FirstComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ React/Cheatsheet/my-app/src/components/FirstComponent.js
@@ -0,0 +1,11 @@
+export default function FirstComponent() {
+    return (
+        <div>
+            <h3>Título</h3>
+            <small>16 de agosto de 2025</small>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit quisquam magni eveniet facilis consequatur ratione cumque voluptatem nobis ducimus quia?</p>
+
+        </div>
+    )
+}
+
